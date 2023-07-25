a5f08e84c6c92649bf07f69c0fc23f7193fe5ec3
keigo mochiduki <keeee50114@gmail.com>
2023-07-25 16:58:34 +0900

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage.jsx";
+import React, { useEffect, useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+
+      <button onClick={onClickSwichShowFlag}>on/pff</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^)/(^^)/</p>}
     </>
   );
 };
